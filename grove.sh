ed90a5c343a1288c9577e572cdde218291cb7062
Matthew Larkin <info@groveos.com>
2022-10-02 13:41:06 -0400

    adds partial files

--- grove.sh
+++ grove.sh
@@ -4,6 +4,23 @@ if [ $1 == 'init' ]; then
 	mkdir embed && mkdir embed/forms embed/partials embed/template embed/vars
 	touch embed/vars/globals.html
 
+	# Partials
+	echo "<cms:block 'meta'>
+</cms:block>
+<title><cms:block 'title'><cms:show k_page_title /></cms:block></title>
+<cms:block 'externals'>
+	<!-- Shoelace -->
+	<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.83/dist/themes/light.css' />
+	<script type='module' src='https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.83/dist/shoelace.js'></script>
+	<!-- Tailwind CSS -->
+	<script src='https://cdn.tailwindcss.com?plugins=typography'></script>
+	<!-- Alpine JS -->
+	<script defer src='https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js'></script>
+</cms:block>" >> embed/partials/head.html
+
+	echo "" >> embed/partials/header.html
+	echo "" >> embed/partials/footer.html
+
 	echo "<html>
 	
 	<cms:block 'global-vars'><cms:embed 'vars/global.html' /></cms:block>
